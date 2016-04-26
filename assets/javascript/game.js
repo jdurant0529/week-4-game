c9bc9c09b9ffc0ee0f16ec64deb60c0b973a0bc1
jdurant0529 <james.durant@gmail.com>
2016-04-26 13:12:55 -0400

    Able to play against 1 character.  cycling to next character doesnt work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,16 +2,16 @@
 	$( document ).ready(function() {
 		var playerChosen = false;
 		var opponentChosen = false;
-
-		//we store all the letters and underscore inside of an array called letters
+		var playerCharacter = [];
+		var opponentCharacter = [];
+		var pcAP = 0;
+		//we store all the available characters in an array titled characters.
 		var characters = [
 		{id: "Obi-Wan-Kenobi", name: "Obi Wan Kenobi", HP: 120, AP:8, CP: 12, image: "<img src='assets/images/obi.jpg'>"},
 		{id: "Darth-Vader", name: "Darth Vader", HP: 100, AP: 10, CP: 10, image: "<img src='assets/images/darth.jpg'>"},
 		{id: "Luke-Skywalker", name: "Luke Skywalker", HP: 80, AP: 12, CP: 8, image: "<img src='assets/images/luke.jpg'>"},
 		{id: "Count-Dooku", name: "Count Dooku", HP: 150, AP: 6, CP: 14, image: "<img src='assets/images/dooku.jpg'>"}];
-		var pcAP = 0;
-
-		// console.log(characters[0].name);
+		
 //i dont really understand this
 		for (var i = 0; i < characters.length; i++) {    
 		    var b = $('<button>');
@@ -22,60 +22,34 @@
 		    b.attr('data-CP', characters[i].CP);
 		    b.attr('data-num', i);
 		    b.attr('id',characters[i].id);
-		    // b.text(characters[i]);
 		    b.html(characters[i].image);
-		    // b.data(characters[i]);
+
 
 		    $("#allchars").append(b); 
 		    //alert('watch this');
 		}
 // end of stuff that I dont really understand.
 
-		// var pc = choosePlayer();
-		// var oc = chooseOpponent();
-
-
-		// function choosePlayer(k){
-		// 	console.log(k + 'I am in choose player function');
-		// 	var playerChosen = true
-		// 	var playerCharacter = characters[k];
-		// 	characters.splice(k,1);
-		// 	return playerCharacter;
-
-		// }
-		// function chooseOpponent(){
-		// 	// var j = prompt('Enter number between 0 and ' + (characters.length-1));
-		// 	var opponentCharacter = characters[$(this).data('num')];
-		// 	characters.splice($(this).data('num'),1);
-		// 	return opponentCharacter;
-		// }
 
 		$('.attackButton').on("click", function(){
-			 alert('Here we are');
 
-			//new stuff
-			
-			// end of new stuff
-
-
-			// var playerCharacter = characters[0];
-			// var opponentCharacter = characters[1];
-			console.log(pc);
-			console.log(oc);	 
-
-			console.log(pcAP);
 			if (pcAP == 0){
-				console.log("current player ="+ pcAP + "player attack power = " + pc.AP);
-				pcAP = pc.AP;
+				console.log("current player ="+ playerCharacter.name +"player attack power = " + playerCharacter.AP);
+				
+				pcAP = playerCharacter.AP;
+				console.log('player current attack power = ' + pcAP)
 			} 
-			oc.HP = oc.HP - pcAP;
-			pcAP = pcAP + pc.AP;
+			opponentCharacter.HP = opponentCharacter.HP - pcAP;
+			pcAP = pcAP + playerCharacter.AP;
 			console.log("Player current attack power = " + pcAP);
-			checkWin(oc.HP, oc.label);
-			pc.HP = pc.HP - oc.CP;
-			console.log(pcAP); 
-			console.log(pc.HP);
-			checkLoss(pc.HP);
+			checkWin(opponentCharacter.HP, opponentCharacter.name);
+			if(opponentCharacter != ''){
+				playerCharacter.HP = playerCharacter.HP - opponentCharacter.CP;
+				console.log(pcAP); 
+				console.log(playerCharacter.HP);
+				checkLoss(playerCharacter.HP);
+			}
+			
 			 // $('player').html('Player choose = ' + oc.label + 'Player health = ' oc.HP + '<br>Player attack Power = ')
 
 		 });  
@@ -87,29 +61,17 @@
 	    		playerChosen = true;
 	    		console.log($(this).data('name'));
 	    		$(this).appendTo($('#player'));
+	    		playerCharacter = characters[$(this).data('num')];
+	    		console.log(playerCharacter);
+
 	    	} else if (opponentChosen == false) {
 	    		opponentChosen = true;
 	    		console.log($(this).data('name'));
 	    		$(this).appendTo($('#opponent'));
+	    		opponentCharacter = characters[$(this).data('num')];
+	    		console.log(opponentCharacter);
 	    	}
 
-	    	// var pc = choosePlayer($(this).data('num'));
-	    	// console.log(pc);
-	    	// $('.allchars').removeClass
-	  //   	for (var i = 0; i < characters.length; i++) {    
-			//     var c = $('<button>');
-			//     c.addClass('character character-button');
-			//     c.attr('data-name', characters[i].name); 
-			//     c.attr('data-HP', characters[i].HP);
-			//     c.attr('data-AP', characters[i].AP);
-			//     c.attr('data-CP', characters[i].CP);
-			//     c.attr('data-num', i);
-			//     // b.text(characters[i]);
-			//     c.html(characters[i].image);
-			//     // b.data(characters[i]);
-
-			//     $("#opponent").append(c); 
-			// }
 		});  
 // end of other stuff i didnt understand
 
@@ -119,7 +81,10 @@
 	    	if(HP<=0){
 	    		if (characters.length > 0) {
 	    			alert("You defeated " + name);
-	    			chooseOpponent();
+	    			opponentChosen = false;
+	    			$('#opponent').appendTo('#defeated');
+	    			opponentCharacter = [];
+	    			console.log(opponentCharacter);
 	    		}else { 
 	    			alert("You defeated " + name);
 	    			alert('You are the master of the universe.');
