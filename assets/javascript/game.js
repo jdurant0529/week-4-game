d608ab79a69d5e9175a58265b1b82b4d29a7ce43
jdurant0529 <james.durant@gmail.com>
2016-04-27 13:58:48 -0400

    cleaned a little up, still too messy for me.  Also still using global variables - learned too late last night to fix that issue. reset game does not work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,18 +6,19 @@
 		var opponentCharacter = [];
 		var pcAP = 0;
 		var counter = 0;
-		var newEnemy = false;
-		var playerStats = '';
-		var opponentStats = '';
+		// var playerStats = '';
+		// var opponentStats = '';
+		var gameOver = false;
 
 		//we store all the available characters in an array titled characters.
 		var characters = [
-		{id: "Obi-Wan-Kenobi", name: "Obi Wan Kenobi", HP: 120, AP:8, CP: 25, image: "<img src='assets/images/obi.jpg'>"},
-		{id: "Darth-Vader", name: "Darth Vader", HP: 100, AP: 10, CP: 5, image: "<img src='assets/images/darth.jpg'>"},
-		{id: "Luke-Skywalker", name: "Luke Skywalker", HP: 80, AP: 12, CP: 20, image: "<img src='assets/images/luke.jpg'>"},
-		{id: "Count-Dooku", name: "Count Dooku", HP: 150, AP: 6, CP: 15, image: "<img src='assets/images/dooku.jpg'>"}];
+		{id: "Obi-Wan-Kenobi", name: "Obi Wan Kenobi", HP: 120, AP:6, CP: 25, image: "<img src='assets/images/obi.jpg'>"},
+		{id: "Darth-Vader", name: "Darth Vader", HP: 100, AP: 4, CP: 5, image: "<img src='assets/images/darth.jpg'>"},
+		{id: "Luke-Skywalker", name: "Luke Skywalker", HP: 80, AP: 8, CP: 20, image: "<img src='assets/images/luke.jpg'>"},
+		{id: "Count-Dooku", name: "Count Dooku", HP: 150, AP: 2, CP: 15, image: "<img src='assets/images/dooku.jpg'>"}];
 		
-//i dont really understand this
+		$('.resetButton').hide();  // hide reset button until game is over.
+
 		for (var i = 0; i < characters.length; i++) {    
 		    var b = $('<button>');
 		    b.addClass('character ' + characters[i].id);
@@ -31,62 +32,60 @@
 
 
 		    $("#allchars").append(b); 
-		    //alert('watch this');
+		  
 		}
-// end of stuff that I dont really understand.
-
 
 		$('.attackButton').on("click", function(){
-			if (playerCharacter.name != null && opponentCharacter.name != null){
-				// console.log(playerCharacter.name +' '+ opponentCharacter.name)
-				// console.log('start of attack');
-				pcAP = pcAP + playerCharacter.AP;
-				// console.log("current player = "+ playerCharacter.name +" player attack power = " + playerCharacter.AP);
-				// console.log('player health = ' + playerCharacter.HP + ' player current attack power = ' + pcAP);
-				pcAP = playerCharacter.AP + pcAP; // add attack power before attack - since current attack power starts at 0.
-		 		// console.log('opponent name = ' + opponentCharacter.name + ' opponent counter-attack = ' + opponentCharacter.CP);
-		 		// console.log('opponent health = ' + opponentCharacter.HP);
-				opponentCharacter.HP = opponentCharacter.HP - pcAP; //opponent loses HP on attack
-				// console.log('after attack, opponent health = ' + opponentCharacter.HP);
-			
+			if (playerCharacter.name !== null && opponentCharacter.name !== null){
 				
+				pcAP = pcAP + playerCharacter.AP;
+			
+				pcAP = playerCharacter.AP + pcAP; // add attack power before attack - since current attack power starts at 0.
 
-				// console.log('1st opponent chosen = ' + opponentChosen);
+				opponentCharacter.HP = opponentCharacter.HP - pcAP; //opponent loses HP on attack
+
 				opponentChosen = checkWin(opponentCharacter.HP, opponentCharacter.name, opponentCharacter.id);
-				// console.log('2nd opponent chosen = ' + opponentChosen);
-				if (opponentChosen == false) {
+				playerCharacter.HP = playerCharacter.HP - opponentCharacter.CP;
+				gameOver = checkLoss(playerCharacter.HP);
+				if (opponentChosen === false) {
 					$('#opponent-stats').empty();
 					opponentCharacter = [];
-				}else{
-				playerCharacter.HP = playerCharacter.HP - opponentCharacter.CP;
-				 // console.log('after counter-attack player health = ' + playerCharacter.HP); 
+					$('#player-stats').html(playerCharacter.name + 
+									'<br> Player Health: ' + playerCharacter.HP +
+									'<br> Player Attack Power: ' + pcAP);
+					$('#opponent-stats').html(opponentCharacter.name + 
+									'<br> Player Health: ' + opponentCharacter.HP + 
+									'<br> Opponent Counter-Attack: ' + opponentCharacter.CP);
+				}else if(gameOver === true) {
+					console.log("Game is over");
+					$('.resetButton').show();
+					$('.attackButton').hide();
+										$('#player-stats').html(playerCharacter.name + 
+									'<br> Player Health: ' + playerCharacter.HP +
+									'<br> Player Attack Power: ' + pcAP);
+					$('#opponent-stats').html(opponentCharacter.name + 
+									'<br> Player Health: ' + opponentCharacter.HP + 
+									'<br> Opponent Counter-Attack: ' + opponentCharacter.CP);
+				} 
+
+				else {
 				
-				// checkLoss(playerCharacter.HP);
-				// playerStats = (playerCharacter.name + 
-				// 					'<br> Player Health: ' + playerCharacter.HP +
-				// 					'<br> Player Attack Power: ' + pcAP);
+				
 				$('#player-stats').html(playerCharacter.name + 
 									'<br> Player Health: ' + playerCharacter.HP +
 									'<br> Player Attack Power: ' + pcAP);
-				// opponentStats = (opponentCharacter.name + 
-				// 					'<br> Player Health: ' + opponentCharacter.HP + 
-				// 					'<br> Opponent Counter-Attack: ' + opponentCharacter.CP);
+				
 				$('#opponent-stats').html(opponentCharacter.name + 
 									'<br> Player Health: ' + opponentCharacter.HP + 
 									'<br> Opponent Counter-Attack: ' + opponentCharacter.CP);
 				}
 			}
-			// }
 			
-			// console.log('end of attack');
-			 // $('player').html('Player choose = ' + oc.label + 'Player health = ' oc.HP + '<br>Player attack Power = ')
-
-		 });  
-		// end of attackButton click
-
-//this I dont understand either
+		 });  // end of attackButton click
+		
 	    $('.character').on('click', function() {
-	    	if(counter == 0 && playerCharacter == false){
+	    	console.log(counter + " " + playerChosen +" " + opponentChosen);
+	    	if(counter === 0 && playerChosen === false){
 	    		counter++;
 				playerChosen = true;
 	    		$(this).appendTo($('#player'));
@@ -100,7 +99,7 @@
 	    							'<br> Player Attack Power: ' + $(this).data('ap'));
 			}else if (counter > 0 && $(this).data('name') == playerCharacter.name){
 	    		alert ('Player can not be opponent');
-	    	}else if (counter > 0 && opponentChosen == false) {
+	    	}else if (counter > 0 && opponentChosen === false) {
 	    		counter++;
 	    		opponentChosen = true;
 	    		$(this).appendTo($('#opponent'));
@@ -112,12 +111,11 @@
 									'<br> Player Health: ' + $(this).data('hp')+
 									'<br> Player Counter-Attack: ' + $(this).data('cp'));
 	    		
-	    	}else if (playerCharacter.name == null || opponentCharacter == null){
+	    	}else if (playerCharacter.name === null || opponentCharacter === null){
 	    		alert ('Both players have yet to be chosen');
 	    	}
 
-		});  
-// end of other stuff i didnt understand
+		});  //end of character button click
 
 	    function checkWin(HP, name, id){
 
@@ -128,40 +126,75 @@
 	    		if (counter < characters.length) {
 	    			alert("You defeated " + name);
 	    			opponentChosen = false;
-	    			// console.log('.character ' + id)
 	    			$('#'+id).appendTo('#defeated');
-	    			// opponentStats = '';
 	    			$('#opponent-stats').empty();
-	    			console.log(opponentCharacter);
-	    			// opponentCharacter = [];
-	    			console.log(opponentCharacter);
-	    			// console.log(opponentCharacter);
-	    		}else if (counter >= characters.length) { 
+	    		}else if (counter >= characters.length) { // if all players chosen (counter) no more enemies to defeat.
 	    			alert("You defeated " + name);
-	    			$('#'+id).appendTo('#defeated');
-	    			opponentStats = '';
-	    			opponentChosen = false;
+	    			$('#'+id).appendTo('#defeated');  //move enemy to defeated area.
+	    			// opponentStats = '';
+	    			opponentChosen = false;  // current opponent is blank.
 	    			//playerStats = ''
-	    			$('#opponent-stats').empty();
+	    			$('#opponent-stats').empty();  //empty the opponent stats div
 	    			// opponentCharacter = [];
-	    			alert('You are the master of the universe.');
-
-	    		} else {
-	    			console.log('I dont know if this ever appears.');
-	    		} 
-	    	} else {
-	    		console.log('Keep fighting same character');
+	    			alert('You are the master of the universe.');  // you win all the gold.
+	    			$('.resetButton').show();  // allow user to reset game.
+	    		}
 	    	}
-	    	// console.log('end of checkWin process');
-	    	return opponentChosen;
-	    }
+	    	return opponentChosen;  //if opponent is defeated -- opponent chosen should be false
+	    }  // end of checkwin
 
 	    function checkLoss(HP){
 	    	console.log("Player health = " + HP);
 	    	if(HP<=0){
 	    		alert("You lost, restart the game.");
+	    		gameOver = true;
+	    		
 	    	}
-
-	    }
+	    	return gameOver;
+	    }  // end of checkLoss
 		
+	    $('.resetButton').on("click", function() {
+			playerChosen = false;
+			opponentChosen = false;
+			playerCharacter = [];
+			opponentCharacter = [];
+			pcAP = 0;
+			counter = 0;
+			// newEnemy = false;
+			// playerStats = '';
+			// opponentStats = '';
+			gameOver = false;
+
+			$('#allchars').empty();
+			$('#defeated').empty();
+			$('#player').empty();
+			$('#opponent').empty();
+			$('#player-stats').empty();
+			$('#opponent-stats').empty();
+
+			for (var i = 0; i < characters.length; i++) {    
+		    var b = $('<button>');
+		    b.addClass('character ' + characters[i].id);
+		    b.attr('data-name', characters[i].name); 
+		    b.attr('data-HP', characters[i].HP);
+		    b.attr('data-AP', characters[i].AP);
+		    b.attr('data-CP', characters[i].CP);
+		    b.attr('data-num', i);
+		    b.attr('id',characters[i].id);
+		    b.html(characters[i].image);
+
+
+		    $("#allchars").append(b); 
+
+		  	$('.resetButton').hide();
+		  	$('.attackButton').show();
+		  	
+		}
+console.log('player chosen = ' + playerChosen + 'opponent chosen = ' + opponentChosen);
+
+	    });
+
+
+
+
 	});
